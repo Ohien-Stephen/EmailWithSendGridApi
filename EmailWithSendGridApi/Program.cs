333100a104c5ee4adff33deb47ce11f65ce185ab
code_stephen <ohienstephen@gmail.com>
2020-08-13 04:11:19 +0100

    Add project files.

---
+++ EmailWithSendGridApi/Program.cs
@@ -0,0 +1,33 @@
+﻿// using SendGrid's C# Library
+// https://github.com/sendgrid/sendgrid-csharp
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using System;
+using System.Threading.Tasks;
+
+namespace Example
+{
+    internal class Example
+    {
+        private static void Main()
+        {
+            Execute().Wait();
+        }
+
+        static async Task Execute()
+        {
+            var apiKey = Environment.GetEnvironmentVariable("SENDGRID_API_KEY");
+            var client = new SendGridClient(apiKey);
+            var from = new EmailAddress("ohienstephen@gmail.com", "Example User");
+            var subject = "Sending with SendGrid is Fun";
+            var to = new EmailAddress("ohienstephen007@gmail.com", "Example User");
+            var plainTextContent = "and easy to do anywhere, even with C#";
+            var htmlContent = "<strong>and easy to do anywhere, even with C#</strong>";
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+            var response = await client.SendEmailAsync(msg);
+
+            Console.WriteLine("____________________________________");
+            Console.WriteLine(response.ToString());
+        }
+    }
+}
